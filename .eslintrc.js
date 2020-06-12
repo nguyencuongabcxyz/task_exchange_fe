0e16bcbca3246658d9e305d4afe063187e90325d
CuongNguyen <Cuong.Nguyen@sioux.asia>
2020-06-12 17:48:41 +0700

    TE_12_06 Update eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,9 @@ module.exports = {
     es6: true,
   },
   extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -22,6 +20,39 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    'linebreak-style': 'off',
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // For more Eslint options, reference here: https://eslint.org/docs/rules/
+    // For more Eslint-Typesciprt plugin options, reference here: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
+    // For more Eslint-React plugin options, reference here: https://github.com/yannickcr/eslint-plugin-react
+    "comma-dangle": ["error", "always-multiline"],
+    "arrow-parens": ["error", "always"],
+    "max-len": ["error", { "code": 120 }],
+    "jsx-quotes": ["error", "prefer-single"],
+    "quotes": ["error", "single"],
+    "object-curly-spacing": ["error", "always"],
+    "indent": "off",
+    "no-param-reassign": "error",
+    "prefer-const": "off",
+    "no-unused-vars": "off",
+    // "@typescript-eslint/indent": ["error", 2],    
+    // "@typescript-eslint/no-unused-vars": ["error", {
+    //   "vars": "all",
+    //   "args": "none",
+    //   "ignoreRestSiblings": false
+    // }],
+    // "@typescript-eslint/no-use-before-define": "off",
+    // "@typescript-eslint/no-var-requires": "warn",
+    // "@typescript-eslint/interface-name-prefix": ["error", "always"],
+    // "@typescript-eslint/array-type": ["error", {
+    //   default: 'array',
+    // }],
+    // "@typescript-eslint/explicit-member-accessibility": ["error", {
+    //   "accessibility": "explicit",
+    //   "overrides": { "constructors": "no-public" }
+    // }],
+    // "@typescript-eslint/no-empty-interface": ["error", {
+    //     "allowSingleExtends": true
+    // }],
+    // "@typescript-eslint/ban-ts-ignore": "off"    
   },
 };
